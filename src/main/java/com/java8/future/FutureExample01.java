f4ef20c757fa92845ca1a0c327af41105f060960
iriwen <worker1301ux@gmail.com>
2020-04-20 10:57:55 +0800

    init commit

---
+++ src/main/java/com/java8/future/FutureExample01.java
@@ -0,0 +1,7 @@
+package com.java8.future;
+
+/**
+ * created by yuxiaodong01 on 2020/04/20.
+ */
+public class FutureExample01 {
+}
---
+++ src/main/java/com/java8/lambda/Circle.java
@@ -0,0 +1,29 @@
+package com.java8.lambda;
+
+public class Circle {
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Circle(String name, int num) {
+        this.name = name;
+        this.num = num;
+    }
+
+    public String name;
+    public int num;
+
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+}
