7cff445daae4f391ca45a5c886b0483fc0242a07
iriwen <worker1301ux@gmail.com>
2020-04-23 11:24:08 +0800

    try with resource

---
+++ src/main/java/com/google/spring/lambda/TryWithResource.java
@@ -0,0 +1,31 @@
+package com.google.spring.lambda;
+
+import java.io.IOException;
+
+/**
+ * created by yuxiaodong01 on 2020/04/23.
+ */
+public class TryWithResource {
+
+    public static void main(String[] args){
+
+       try(MyCloseAble closeAble = new MyCloseAble()){
+           closeAble.sendData();
+       }catch(Exception e){
+           e.printStackTrace();
+       }
+    }
+}
+
+class MyCloseAble implements AutoCloseable{
+
+    public void sendData(){
+        System.out.println("is sending data");
+    }
+
+    @Override
+    public void close() throws IOException {
+        System.out.println("is closing connection !");
+    }
+}
+
