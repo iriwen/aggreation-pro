f862df8b3ed8812ffdfd08ce85ba1ef8db501531
iriwen <worker1301ux@gmail.com>
2020-05-10 14:38:55 +0800

    单例模式完善

--- src/main/java/com/java/code/designPattern/Singleton.java
+++ src/main/java/com/java/code/designPattern/Singleton.java
@@ -7,13 +7,13 @@ public class Singleton {
 
     private static Singleton instance;
 
-    private Singleton() {
-    }
+    private Singleton() {}
 
     public static Singleton getInstance() {
         if (instance == null) {
 
             synchronized (Singleton.class) {
+                //加锁之后再判断是不是为空，在获得锁之前可能被其他线程实例化
                 if (instance == null) {
                     instance = new Singleton();
                 }
