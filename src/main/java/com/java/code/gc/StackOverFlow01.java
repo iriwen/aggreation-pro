a927d9c14b61a85b44386826a8fbcc6e8d1bd837
iriwen <worker1301ux@gmail.com>
2020-06-10 10:21:34 +0800

    堆栈溢出

---
+++ src/main/java/com/java/code/gc/StackOverFlow01.java
@@ -0,0 +1,38 @@
+package com.java.code.gc;
+
+/**
+ * created by yuxiaodong01 on 2020/06/10.
+ *
+ * StackOverflow
+ * <Description> StackOverflowError<br>
+ */
+public class StackOverFlow01 {
+
+    private final StackOverFlow02 b;
+
+    public StackOverFlow01() {
+        b = new StackOverFlow02();
+    }
+
+    public static void main(String[] args) {
+        StackOverFlow01 instance = new StackOverFlow01();
+        instance.stackA();
+    }
+
+    public void stackA() {
+        b.stackB();
+    }
+}
+
+class StackOverFlow02 {
+
+    private final StackOverFlow01 a;
+
+    public StackOverFlow02() {
+        a = new StackOverFlow01();
+    }
+
+    public void stackB() {
+        a.stackA();
+    }
+}
