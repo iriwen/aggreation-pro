cee8ba7aeb93addc664c114d546354ac9b47ae88
iriwen <worker1301ux@gmail.com>
2020-07-12 23:52:37 +0800

    es 依赖引入

---
+++ src/main/java/com/java/code/zookeeper/DistributedLock.java
@@ -0,0 +1,44 @@
+package com.java.code.zookeeper;
+
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.RetryNTimes;
+
+/**
+ * 利用zk实现分布式锁
+ * ReentranLock（true） 表示是公平锁
+ */
+public class DistributedLock {
+
+    // 此demo使用的集群，所以有多个ip和端口
+    private static final String ZK_SERVER = "localhost:2181";
+    // session过期时间
+    private static final int SESSION_TIMEOUT = 3000;
+    // 连接超时时间
+    private static final int CONNECTION_TIMEOUT = 3000;
+    // 锁节点
+    private static final String LOCK_NODE = "/distributeLock";
+
+
+    public static void main(String[] args) {
+
+        RetryNTimes retryPolicy = new RetryNTimes(10, 5000);
+        CuratorFramework client = CuratorFrameworkFactory.builder().
+                connectString(ZK_SERVER).
+                connectionTimeoutMs(15 * 1000).
+                sessionTimeoutMs(60 * 100).
+                retryPolicy(retryPolicy).
+                build();
+        client.start();
+
+
+
+        //CuratorFramework framework = CuratorFrameworkFactory.
+         //       newClient(ZK_SERVER, SESSION_TIMEOUT, CONNECTION_TIMEOUT, new RetryNTimes(10,5000));
+
+
+    }
+
+
+
+}
--- src/main/java/com/java/code/zookeeper/ZkWatcher.java
+++ src/main/java/com/java/code/zookeeper/ZkWatcher.java
@@ -7,6 +7,9 @@ import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.ZooKeeper;
 import org.apache.zookeeper.data.Stat;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /**
  * created by yuxiaodong01 on 2020/07/12.
  */
@@ -18,8 +21,8 @@ public class ZkWatcher implements Watcher {
     public static void main(String[] args) throws Exception {
 
         //这里的watcher不指定好，启动时候会有空异常报错
-        zk = new ZooKeeper("localhost:2181", 3000, item->{
-            System.out.println(" zk up ; the state is " + item.getState());
+        zk = new ZooKeeper("localhost:2181", 3000, item -> {
+            System.out.println("the zookeeper server up ; the state is " + item.getState());
         });
         ZkWatcher watcher = new ZkWatcher();
         Stat exists = zk.exists(ZkWatcher.path, watcher);
@@ -28,18 +31,24 @@ public class ZkWatcher implements Watcher {
         } else {
             System.out.println("target znode exists !");
         }
+
         while (true) {
-
-
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String input = reader.readLine();
+            System.out.println("用户完成输入： " + input);
+            if ("exit".equals(input)) {
+                System.exit(0);
+            } else {
+                System.out.println("请输入 exit 来结束当前进程; 当前输入是:" + input);
+            }
         }
     }
 
-
-
     /**
-     * 每当一个watcher处理完，被服务端移除之后，执行的对应的process方法会再重新注册新的watcher ，持续对node节点新发生的事件进行关注，
-     * 注意设置好和zookeeper的会话失效时间（这里是关键，否则进行watcher注册的时候会提示connection losed，导致注册失败）,
-     * 连接保持好 则持续注册都会成功
+     * 每当一个watcher处理完，被服务端移除之后，执行的对应的process方法会再重新注册新的watcher，
+     * 持续对node节点新发生的事件进行关注，注意设置好和zookeeper的会话失效时间（这里是关键，
+     * 否则进行watcher注册的时候会提示connection losed，导致注册失败）,连接保持好 则持续注册都会成功
+     *
      * @param watchedEvent
      */
     @Override
@@ -57,4 +66,6 @@ public class ZkWatcher implements Watcher {
             System.out.println("exception info :" + e.getMessage());
         }
     }
+
+
 }
