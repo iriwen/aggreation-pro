11c520328f5ffbc1dc7453fc9d0e31f126d8e120
iriwen <worker1301ux@gmail.com>
2020-07-21 10:04:11 +0800

    获取二叉树深度

---
+++ src/main/java/com/java/code/leetcode/TreeDeep.java
@@ -0,0 +1,12 @@
+package com.java.code.leetcode;
+
+import java.util.LinkedList;
+
+public class TreeDeep {
+
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+    }
+}
