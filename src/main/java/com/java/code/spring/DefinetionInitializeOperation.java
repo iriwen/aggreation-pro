ccf3a221f13a1fcf32693bf471355b8265b0aea3
yuxiaodong <worker1301ux@gmail.com>
2021-02-26 10:48:42 +0800

    相关操作修复

--- src/main/java/com/java/code/spring/DefinetionInitializeOperation.java
+++ src/main/java/com/java/code/spring/DefinetionInitializeOperation.java
@@ -6,27 +6,27 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * created by yuxiaodong01 on 2021/02/26.
  * 注解传递
  *
  */
-
 @ManjaroService
 public class DefinetionInitializeOperation implements InitializingBean {
 
     private final Logger logger = LoggerFactory.getLogger(ExcelOperationController.class);
 
-    private final List<String> list = new ArrayList<>();
+    private List<String> list ;
 
     @Override
     public void afterPropertiesSet() throws Exception {
         String[] strs = {"a", "b"};
-        Collections.addAll(list, strs);
+        list = Stream.of(strs).collect(Collectors.toList());
+        //Collections.addAll(list, strs);
         logger.info("list size : {}",list.size());
         MyApplicationListener myApplicationListener = ApplicationContextHelper.getBean(MyApplicationListener.class);
         logger.info("get bean from application Context :{}", myApplicationListener, getClass());
