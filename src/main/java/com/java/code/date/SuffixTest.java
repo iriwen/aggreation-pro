2a6e98a00e3c8ba27e38b06e31978fd564c907c3
yuxiaodong <worker1301ux@gmail.com>
2021-04-09 18:06:01 +0800

    排序和字符串后缀

--- src/main/java/com/java/code/date/SuffixTest.java
+++ src/main/java/com/java/code/date/SuffixTest.java
@@ -11,5 +11,11 @@ public class SuffixTest {
         String path = "/home/worker/temp/messageNotifyMobiles_aa318492-3f6c-438b-ad15-ee962a942c7e.xlsx";
         String suffix = path.substring(path.lastIndexOf("."));
         System.out.println(suffix);
+
+
+        String initStr = "sdsdsd$null$null$sdsd";
+        String replace = initStr.replace("null", "");
+        System.out.println("after replace :" + replace);
     }
 }
+
